09585278bbfee66589aee1c92001a1e03fd3c36e
Endre Bakken Stovner <endrebak@idi.ntnu.no>
2015-07-27 19:30:17 +0200

    now gets ref genome data correctly
    amend: returned as df

--- snp_flip/reference_genome.py
+++ snp_flip/reference_genome.py
@@ -5,17 +5,29 @@ from sys import stderr
 
 from operator import itemgetter
 
+import pandas as pd
+
 from pyfaidx import Fasta
 
-def get_reference_genome_data(snp_positions, chromosome, genome_fasta):
 
-    _get_snps = itemgetter(*snp_positions)
-    chrom_data = Fasta(genome_fasta, as_raw=True, read_ahead=100000)[chromosome]
-    snp_nucleotides = [snp.upper() for snp in _get_snps(chrom_data)]
 
-    _check_for_N(snp_nucleotides, chromosome)
+def get_reference_genome_data(bim_table, genome_fasta):
 
-    return snp_nucleotides
+    pyfaidx_genome = Fasta(genome_fasta, as_raw=True, read_ahead=100000)
+    genome_dict = convert_fa_chromosome_names(pyfaidx_genome)
+
+    chromosome_data = []
+    for chromosome, nucleotides in genome_dict.items():
+
+        positions = bim_table[bim_table["chromosome"] == chromosome]["position"]
+        _get_snps = itemgetter(*list(positions))
+
+        snp_nucleotides = [snp.upper() for snp in _get_snps(str(nucleotides))]
+        chromosome_data.append(pd.Series(snp_nucleotides))
+
+        _check_for_N(snp_nucleotides, chromosome)
+
+    return pd.concat(chromosome_data)
 
 def _check_for_N(snps, chromosome):
 
--- tests/test_get_reference_genome_data.py
+++ tests/test_get_reference_genome_data.py
@@ -1,15 +1,29 @@
 import pytest
 
+from numpy import array_equal
+import pandas as pd
+
+
+from ebs.imports import StringIO
 
 
 from snp_flip.reference_genome import get_reference_genome_data
 
 def describe_get_reference_genome_data():
 
-    def test_get_data(snp_positions, genome_fasta):
-        snp_nucleotides = get_reference_genome_data(snp_positions, "chr1", str(genome_fasta))
-        assert snp_nucleotides == [u"A", u"T", u"C"]
+    def test_get_data(bim_df, genome_fasta, expected_result):
+        snp_nucleotides = get_reference_genome_data(bim_df, str(genome_fasta))
+        print(snp_nucleotides)
+        print(type(snp_nucleotides))
+        print(expected_result)
+        print(type(expected_result))
+        assert array_equal(expected_result, snp_nucleotides)
 
+
+@pytest.fixture
+def expected_result():
+    return pd.read_table(StringIO("""A\nT\nC\nA\n"""), squeeze=True,
+                         header=None)
 
 
 @pytest.fixture
@@ -19,6 +33,18 @@ def genome_fasta(tmpdir):
     genome_fa.write(u""">chr1\nACGTTCCG\n>chr2\nAAC\n""")
     return genome_fa
 
+
 @pytest.fixture
-def snp_positions():
-    return [0, 3, 5]
+def bim_string():
+    return """1 	rs11511647	0	0	A	T
+1	rs3883674	0	3	C	G
+1	rs12218882	0	5	T	T
+2	rs12218882	0	1	T	T"""
+
+@pytest.fixture
+def bim_df(bim_string):
+
+    return pd.read_table(StringIO(bim_string), sep=r"\s+", header=None,
+                         names=["chromosome", "snp_name", "genetic_distance",
+                                "position", "allele_1", "allele_2"],
+                         dtype={"chromosome": str})
